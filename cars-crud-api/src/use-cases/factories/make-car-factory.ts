afd4e3ddc6f3c6f813fc9f0a5a80f205223f02ff
Pedro Veras <veraspedro1608@gmail.com>
2024-05-13 20:19:30 -0300

    feat: create more cars factory

--- cars-crud-api/src/use-cases/factories/make-car-factory.ts
+++ cars-crud-api/src/use-cases/factories/make-car-factory.ts
@@ -1,5 +1,9 @@
 import { CreateCarUseCase } from '../create-car-use-case'
 import { PrismaCarsRepository } from '@/repositories/prisma/prisma-cars-repository'
+import { DeleteCarUseCase } from '../delete-car-use-case'
+import { GetAllCarsUseCase } from '../get-all-cars-use-case'
+import { GetByIdCarUseCase } from '../get-by-id-car-use-case'
+import { UpdateCarUseCase } from '../update-car-use-case'
 
 const carsRepository = new PrismaCarsRepository()
 
@@ -7,4 +11,28 @@ export function MakeCreateCarUseCase() {
   const createCarUseCase = new CreateCarUseCase(carsRepository)
 
   return createCarUseCase
+}
+
+export function MakeGetAllCarsUseCase() {
+  const getAllCarsUseCase = new GetAllCarsUseCase(carsRepository)
+
+  return getAllCarsUseCase
+}
+
+export function MakeGetByIdCarsUseCase() {
+  const getByIdCarUseCase = new GetByIdCarUseCase(carsRepository)
+
+  return getByIdCarUseCase
+}
+
+export function MakeUpdateCarUseCase() {
+  const updateCarUseCase = new UpdateCarUseCase(carsRepository)
+
+  return updateCarUseCase
+}
+
+export function MakeDeleteCarUseCase() {
+  const deleteCarUseCase = new DeleteCarUseCase(carsRepository)
+
+  return deleteCarUseCase
 }
